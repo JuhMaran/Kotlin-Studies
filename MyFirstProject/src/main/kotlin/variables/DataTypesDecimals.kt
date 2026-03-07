ba1196a56418824725d8eec9e7ce21fc641baa19
JuhMaran <julianemaran@gmail.com>
2026-03-07 20:19:52 -0300

    data types decimals

---
+++ MyFirstProject/src/main/kotlin/variables/DataTypesDecimals.kt
@@ -0,0 +1,18 @@
+package variables
+
+fun main() {
+
+    /*
+    Float: 4 bytes ou 32 bits
+    Double: 8 bytes ou 64 bits
+     */
+
+    // Double
+    val d1 = 14.5
+    val d2: Double = 5.0
+
+    // Float
+    val f1 = 14.5F // sufixo 'F' ou 'f'
+    val f2: Float = 32.5F
+
+}
