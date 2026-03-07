66e0e78eb3fe928707f562639906f932b889aaf4
JuhMaran <julianemaran@gmail.com>
2026-03-07 16:22:21 -0300

    data types integers

---
+++ MyFirstProject/src/main/kotlin/variables/DataTypesIntegers.kt
@@ -0,0 +1,22 @@
+package variables
+
+fun main() {
+
+    /*
+     Byte: 1 byte - 8 bits
+     Short: 2 bytes - 16 bits
+     Int: 4 bytes - 32 bits (mais utilizado)
+     Long: 8 bytes - 64 bits (mais utilizado)
+     */
+
+    // Int
+    val i1 = 10 // assume Int como padrão
+    val i2: Int = 10 // inicializa e declara
+    val i3: Int // declarar o tipo
+
+    // Long
+    val l1 = 10 // assume como Int por padrão
+    val l2 = 10L // força a declarar como Long
+    val l3 = 1234567890123 // Incluir numeração longa, entende que é uma variável do tipo Long
+
+}
