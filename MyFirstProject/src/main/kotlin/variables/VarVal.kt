2d74c30cf9615de0180115f655c8dd2ffe596f47
JuhMaran <julianemaran@gmail.com>
2026-03-07 16:12:02 -0300

    variaveis val e var

---
+++ MyFirstProject/src/main/kotlin/variables/VarVal.kt
@@ -0,0 +1,20 @@
+package variables
+
+fun main() {
+
+//    var counter: Int // define o tipo
+//    counter = 10 // inicializa a variável
+//
+//    var counter: Int = 10
+//
+//    var counter = 10 // mais utilizada
+//
+//    var counter // não compila - forma errada - é necessário inicializar a variável ou atribuir um valor
+
+    var counter = 10
+    counter = 5 // substitui o valor da variável
+
+    val counter2 = 10 // assume o valor como Integer
+    // counter2 = 5 // 'val' cannot be reassigned.
+
+}
